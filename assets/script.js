e13a3d3ae8d71fab97566de13661e720b866499c
Alex <alex.streeter@airlockdigital.com>
2023-10-29 18:35:30 +1030

    Fixed up issues with build

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,9 @@ WebAssembly.instantiateStreaming(fetch("main.wasm"), goWasm.importObject)
 .then((result) => {
     goWasm.run(result.instance)
 
+    const body = document.getElementsByTagName("BODY")[0];
+    body.innerHTML = getHtml();
+
 })
 
 
--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
 
-// var htmlString = "<h2>Nothing like some html in a string</h2>"
+var htmlString = "<h2>Nothing like some html in a string</h2>"
 
-// func GetHtml() js.Func {
-// 	return js.FuncOf(func(this js.Value, args []js.Value) any {
-// 		return htmlString
-// 	})
-// }
+func GetHtml() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		return htmlString
+	})
+}
 
 func main() {
+	ch := make(chan bool)
 	fmt.Printf("see if this still works")
+	js.Global().Set("getHtml", GetHtml())
+	<-ch // block indefinitely to keep program alive
 }

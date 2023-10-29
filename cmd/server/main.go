bb3f4e8d073a4fc7afe58abc6e6a7db565a4862d
Alex <alex.streeter@airlockdigital.com>
2023-10-29 17:30:42 +1030

    Updated to have working wasm site

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := ":3000"
+	log.Printf("listening on port %v", port)
+	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir("../../assets"))))
+
+}
